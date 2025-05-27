98a89b87e51d381a3491c7fb4e257f9d573dbb69
tommaso-mandorino <tommasomandorino@outlook.it>
2025-05-27 17:04:52 +0200

    feat: add user data acquisition

---
+++ main.js
@@ -0,0 +1,17 @@
+// #region Constants declaration section
+
+    // Defined price for 1 km
+    const KM_PRICE = 0.21;
+
+// #endregion Constants declaration section
+
+
+// #region User data obtaining section
+
+    // Get kilometer number from the user
+    const kmNumber = Number(prompt('Inserisci il numero di km che vuoi percorrere:'));
+
+    // Get passenger age from the user
+    const passengerAge = Number(prompt('Inserisci l\'età del passeggero:'));
+
+// #endregion User data obtaining section
